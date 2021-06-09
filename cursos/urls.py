c205b8ea189af2e8370639b37bafcade3359a527
CL16006 <54723993+CL16006@users.noreply.github.com>
2021-06-09 11:38:54 -0600

    se agrego url para admin

--- cursos/urls.py
+++ cursos/urls.py
@@ -3,10 +3,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('home', views.home, name='home'),
+    path('', views.home, name='home'),
     path('index',views.index,name='index'),
     path('verCursos',views.verCursos,name='verCursos'),
     path('contacto',views.contacto,name='contacto'),
     path('inscribirse',views.inscribirse,name='inscribirse'),
-    path('quienesSomos',views.quienesSomos,name='quienesSomos')
+    path('quienesSomos',views.quienesSomos,name='quienesSomos'),
+    path('administrar',views.admin,name='administrar')
     ]
