3a0038fd4ff679d28fed4079b88a83a889dac7e4
CL16006 <54723993+CL16006@users.noreply.github.com>
2021-06-17 18:05:13 -0600

    Agregue la url a la raiz.

--- CasaCulturaWeb/urls.py
+++ CasaCulturaWeb/urls.py
@@ -20,6 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('casacultura/', include('cursos.urls')),
+    path('', include('cursos.urls')),
 ]
 urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
