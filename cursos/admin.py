c1d832c8ce31c2e621e9fff13ff1d4486ea259ef
CL16006 <54723993+CL16006@users.noreply.github.com>
2021-06-19 21:30:31 -0600

    se mejoro la interfaz de usuario

--- cursos/admin.py
+++ cursos/admin.py
@@ -4,10 +4,12 @@ from .models import cursos, anuncio,inscripcion
 class CursoAdmin(admin.ModelAdmin):
     list_display = ["nombre","descripcion","fecha_inicio","fecha_fin"]
     search_fields = ["nombre"]
+    list_filter = ["nombre"]
     
 class AnuncioAdmin(admin.ModelAdmin):
     list_display = ["encabezado","texto","fecha_publicacion"]
     search_fields = ["encabezado"]
+    list_filter = ["encabezado"]
     
 class InscripcionAdmin(admin.ModelAdmin):
     list_display = ["nombres","apellidos","telefono","email"]
